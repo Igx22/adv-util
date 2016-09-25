993147a973bb7b458e677a4861b05881170221a6
igor <direct500>
2016-09-25 23:28:23 +0300

    рпч демо с местом на диске

--- src/main/java/adv/util/TimeUtil.java
+++ src/main/java/adv/util/TimeUtil.java
@@ -9,6 +9,7 @@ import java.time.temporal.ChronoField;
 import java.util.Date;
 
 public class TimeUtil {
+    public static final DateTimeFormatter FORMATTER_DDMMM_HHMM = DateTimeFormatter.ofPattern("dd.MMM HH:mm");
     public static final DateTimeFormatter FORMATTER_DDMMYYYY = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public static final DateTimeFormatter FORMATTER_DDMM = new DateTimeFormatterBuilder().appendPattern("dd.MM")
             .parseDefaulting(ChronoField.YEAR, LocalDate.now().getYear())
