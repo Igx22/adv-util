2b2635ba24c08ed7516cb553cd9168e2fa5fea3c
i.artemyev <swein2@gmail.com>
2016-12-05 17:46:32 +0300

    [dsp] limits V2 + basic undertow/resteasy/jackson/

--- src/main/java/adv/util/Encodable.java
+++ src/main/java/adv/util/Encodable.java
@@ -1,12 +1,15 @@
 package adv.util;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public interface Encodable {
 
     String token();
 
-    public static <E extends Enum<E> & Encodable> E forToken(Class<E> cls, String tok) {
+    static <E extends Enum<E> & Encodable> E forToken(Class<E> cls, String tok) {
         final String t = tok.trim().toUpperCase();
         return Stream.of(cls.getEnumConstants())
                 .filter(e -> e.token().equals(t))
@@ -14,4 +17,17 @@ public interface Encodable {
                 .orElseThrow(() -> new IllegalArgumentException("Unknown token '" +
                         tok + "' for enum " + cls.getName()));
     }
+
+    static <E extends Encodable> Map<String, E> tokenMap(Class<E> cls) {
+        return Stream.of(cls.getEnumConstants()).collect(Collectors.toMap(Encodable::token, Function.identity()));
+    }
+
+    /*
+    это не работает - похоже на баг компилятора
+    https://bugs.openjdk.java.net/browse/JDK-8141508
+
+    public static <E extends Enum<E> & Encodable> Map<String, E> tokenMap(Class<E> cls) {
+        return Stream.of(cls.getEnumConstants()).collect(Collectors.toMap(Encodable::token, Function.identity()));
+    }
+    */
 }
