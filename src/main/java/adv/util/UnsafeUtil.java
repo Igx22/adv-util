5ceb207d44a84512368d650e1604c9292110ca2f
i.artemyev <swein2@gmail.com>
2017-08-07 16:19:40 +0300

    [dsp] dsp ad generator

--- src/main/java/adv/util/UnsafeUtil.java
+++ src/main/java/adv/util/UnsafeUtil.java
@@ -1,17 +1,6 @@
 package adv.util;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
 public class UnsafeUtil {
 
-    public static void setFinalStatic(Field field, Object newValue) throws Exception {
-        field.setAccessible(true);
 
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-
-        field.set(null, newValue);
-    }
 }
