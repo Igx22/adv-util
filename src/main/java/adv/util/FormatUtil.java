78fdb39829d94ef2b21cefd9f9b55a8a73661f4f
i.artemyev <swein2@gmail.com>
2017-09-19 20:26:09 +0300

    [dsp][ssp] log cleanup

--- src/main/java/adv/util/FormatUtil.java
+++ src/main/java/adv/util/FormatUtil.java
@@ -9,4 +9,8 @@ public class FormatUtil {
     public static String formatDouble2Digits(double value) {
         return new DecimalFormat("0.00", new DecimalFormatSymbols(Locale.US)).format(value);
     }
+
+    public static String formatDouble5Digits(double value) {
+        return new DecimalFormat("0.00000", new DecimalFormatSymbols(Locale.US)).format(value);
+    }
 }
