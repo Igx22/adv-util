fd9995b3f2c0c957e789ddb72fdfa4cee39864b2
i.artemyev <swein2@gmail.com>
2017-10-02 22:27:53 +0300

    disk serializer refactored

--- src/main/java/adv/util/DiskSerializer.java
+++ src/main/java/adv/util/DiskSerializer.java
@@ -6,44 +6,49 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Утилита которая
  * 1) сохраняет и загружает атомарно файл с диска
  * 2) ведет архив старых версий файла (файлы заданной давности удаляются)
  * 3) прямой доступ к файлу заменяется на synchronized + callbacks
- *
+ * <p>
  * singleton!!!, только один инстанс на файл.
- *
+ * <p>
  * todo можно лочить файл средствами ос если нужно
- *
+ * <p>
  * атомарность реализована след образом
  * --------------
  * 1) пишем в tmp файл, потом "атомарно" переименовываем в основной
  * 2) основной файл не удаляем а переименовываем в архивный
  * 3) все файлы старше х дней - удаляем
- *
+ * <p>
  * все попытки rename/move производятся много раз с *большими* таймаутами
  */
 public class DiskSerializer {
     private static final Logger log = LoggerFactory.getLogger(DiskSerializer.class);
-    // сколько мы храним бэкапы топологии
+    // сколько мы храним бэкапы
     private final long keepOldFilePeriod;
     // todo это ужасный костыль, но т.к. в production с файлами творится неизвестно что
     // "повторы" гарантируют что с высокой вероятностью нам удастся сделать с файлом то что нужно в конечном итоге
     private static final int FILE_OP_RETRY_COUNT = 15;
     private static final int FILE_OP_RETRY_DELAY = 2000;
     private static final int FILE_BUFFER_SIZE = 50000;
-    public static final int AVERAGE_TOPOLOGY_SIZe = 15 * 1024 * 1024;
     protected final File storageDir;
     protected final File storageFile;
+
+
+    public DiskSerializer(String storageDirPath, String fileName) {
+        this(storageDirPath, fileName, 0, TimeUnit.MILLISECONDS);
+    }
 
     public DiskSerializer(String storageDirPath, String fileName, long keepOldFilePeriod, TimeUnit timeUnit) {
         try {
             this.keepOldFilePeriod = timeUnit.toMillis(keepOldFilePeriod);
             this.storageDir = FileUtil.mkDirOrFail(storageDirPath);
             this.storageFile = new File(storageDir, fileName);
-            log.debug("using topology directory: {}", storageDir.getAbsolutePath());
+            log.debug("using directory: {}", storageDir.getAbsolutePath());
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
@@ -68,11 +73,15 @@ public class DiskSerializer {
         InputStream is = null;
         try {
             clock.start();
-            is = new BufferedInputStream(new FileInputStream(storageFile), FILE_BUFFER_SIZE);
-            reader.read(is);
-            log.info("reading {} Kib from file: {} time: {}s",
-                    String.format("%3.1f", storageFile.length() / 1024.0), storageFile.getAbsolutePath(),
-                    String.format("%3.3f", clock.getTime() / 1000.0));
+            if (!storageFile.exists()) {
+                log.info("missing file {}", storageFile);
+            } else {
+                is = new BufferedInputStream(new FileInputStream(storageFile), FILE_BUFFER_SIZE);
+                reader.read(is);
+                log.info("reading {} Kib from file: {} time: {}s",
+                        String.format("%3.1f", storageFile.length() / 1024.0), storageFile.getAbsolutePath(),
+                        String.format("%3.3f", clock.getTime() / 1000.0));
+            }
         } catch (Exception e) {
             throw new IllegalStateException(e);
         } finally {
@@ -83,6 +92,18 @@ public class DiskSerializer {
                 }
             }
         }
+    }
+
+    public void write(byte[] data) {
+        write(os -> os.write(data));
+    }
+
+    public byte[] read() {
+        AtomicReference<byte[]> result = new AtomicReference<>(null);
+        read(is -> {
+            result.set(StreamUtils.read(is));
+        });
+        return result.get();
     }
 
     public File getStorageFile() {
