acc3eb31098276d45b0816445ec3d70fcc010611
Michael Gamov <nofater@gmail.com>
2019-01-23 17:31:44 +0300

    money formatting

--- src/main/java/adv/util/FormatUtil.java
+++ src/main/java/adv/util/FormatUtil.java
@@ -17,4 +17,8 @@ public class FormatUtil {
     public static String formatDouble5Digits(double value) {
         return new DecimalFormat("0.00000", new DecimalFormatSymbols(Locale.US)).format(value);
     }
+
+    public static String formatDouble5DigitsSoft(double value) {
+        return new DecimalFormat("0.#####", new DecimalFormatSymbols(Locale.US)).format(value);
+    }
 }
