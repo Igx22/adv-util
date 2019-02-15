4eed00bc9d37472fc70d714ba04dd171d697b813
i.artemyev <swein2@gmail.com>
2019-02-15 20:20:32 +0300

    [dsp] для каждого фида: можно задавать процент траффика который он получает (0-100), с какими ssp он может торговать и какой endpoint вызывать в зависимости от ssp

--- src/main/java/adv/util/RandomUtil.java
+++ src/main/java/adv/util/RandomUtil.java
@@ -8,5 +8,15 @@ public class RandomUtil {
         Check.isTrue(high > low);
         return low + rnd.nextInt(high - low);
     }
+
+    public static boolean getTrueWithProbability(Random rnd, double probability) {
+        if (probability >= 0.99999d) {
+            return true;
+        }
+        if (probability <= 0.00001d) {
+            return false;
+        }
+        return rnd.nextDouble() <= probability;
+    }
 }
 
