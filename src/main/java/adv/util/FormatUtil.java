b9565366a8d8c3c087745f05c6af72273ee22e80
iartmyev <swein2@gmail.com>
2020-05-22 18:42:30 +0300

    Format8digits

--- src/main/java/adv/util/FormatUtil.java
+++ src/main/java/adv/util/FormatUtil.java
@@ -33,6 +33,13 @@ public class FormatUtil {
         }
     };
 
+    private static ThreadLocal<DecimalFormat> d8s = new ThreadLocal<>() {
+        @Override
+        protected DecimalFormat initialValue() {
+            return new DecimalFormat("0.########", new DecimalFormatSymbols(Locale.US));
+        }
+    };
+
     public static String formatDouble2Digits(double value) {
         return d2.get().format(value);
     }
@@ -47,5 +54,9 @@ public class FormatUtil {
 
     public static String formatDouble5DigitsSoft(double value) {
         return d5s.get().format(value);
+    }
+
+    public static String formatDouble8DigitsSoft(double value) {
+        return d8s.get().format(value);
     }
 }
